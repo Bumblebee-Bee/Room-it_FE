545ccc52c853793e9adc01ebd54ad11275343a02
JOEIH <joh73459@gmail.com>
2025-01-10 17:01:31 +0900

    💄 Design : 사용자, 사업자 알림페이지에 3달 지난 알림은 삭제처리된다는 문구 추가

--- src/pages/UserNotiPage/components/UserNotiList.tsx
+++ src/pages/UserNotiPage/components/UserNotiList.tsx
@@ -1,4 +1,5 @@
 import { SyncLoader } from 'react-spinners';
+import { AiFillInfoCircle } from 'react-icons/ai';
 import UserNotiCard from './UserNotiCard';
 import useGetUserAlarm from '../hooks/useGetUserAlarm';
 
@@ -13,6 +14,11 @@ const UserNotiList = () => {
     <>
       {sortedNotification && sortedNotification.length > 0 ? (
         <div className='mt-4 flex w-[375px] flex-col justify-center gap-[13px] pb-24'>
+          <div className='mx-auto flex w-custom items-center gap-1 self-start pl-[6px] text-sm text-subfont'>
+            <AiFillInfoCircle />
+            3달이 경과된 알림은 자동 삭제처리됩니다.
+          </div>
+          <hr className='mx-[22.5px] w-custom border-[0.5px] border-dashed' />
           {sortedNotification.map((item) => {
             return (
               <UserNotiCard
