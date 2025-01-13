06b51818178f349dcb9d71f2808c4da71972924d
JOEIH <joh73459@gmail.com>
2025-01-13 23:58:18 +0900

    ♻️ Refactor : 렌더링 시 isLoading 우선순위 변경 #159

--- src/pages/ManagementReserverPage/hooks/useGetReserver.ts
+++ src/pages/ManagementReserverPage/hooks/useGetReserver.ts
@@ -1,18 +1,23 @@
 import { getAllReserver } from '@apis/reservation';
 import { useQuery } from '@tanstack/react-query';
+import { ReserverInfo } from '@typings/types';
 
 const useGetReserver = () => {
   const { data, isLoading } = useQuery({
     queryKey: ['reserver'],
     queryFn: async () => {
+      let reserverData: ReserverInfo[] = [];
       const reserverList = await getAllReserver();
-      const filteredReserverList = reserverList.filter(
-        (item) =>
-          item.reservationState !== 'ON_HOLD' &&
-          item.reservationState !== 'PAYMENT_FAIL',
-      );
 
-      return filteredReserverList;
+      if (reserverList) {
+        reserverData = reserverList.filter(
+          (item) =>
+            item.reservationState !== 'ON_HOLD' &&
+            item.reservationState !== 'PAYMENT_FAIL',
+        );
+      }
+
+      return reserverData;
     },
   });
   return { reserverList: data ?? [], isLoading };
