f20792b5d51554f3b54ddf2bf8561f60027002ff
eunjju2 <graceun309@gmail.com>
2025-01-15 10:27:45 +0900

    ♻️ Refactor : 토큰 만료 시 화면 전환 개선 중 #164

--- src/pages/UserMypage/index.tsx
+++ src/pages/UserMypage/index.tsx
@@ -1,20 +1,23 @@
 import HeaderNoTitle from '@layouts/HeaderNoTitle';
 import MainLayout from '@layouts/MainLayout';
 import BottomNavigation from '@layouts/BottomNavigation';
+import TokenRefresher from '@components/TokenRefresher';
 import UserButtonContainer from './components/UserButtonContainer';
 import UserInfo from './components/UserInfo';
 
 const UserMypage = () => {
   return (
     <>
-      <MainLayout>
-        <HeaderNoTitle />
-        <div className='flex h-[263px] w-[375px] flex-col items-center bg-primary'>
-          <UserInfo />
-          <UserButtonContainer />
-        </div>
-        <BottomNavigation />
-      </MainLayout>
+      <TokenRefresher>
+        <MainLayout>
+          <HeaderNoTitle />
+          <div className='flex h-[263px] w-[375px] flex-col items-center bg-primary'>
+            <UserInfo />
+            <UserButtonContainer />
+          </div>
+          <BottomNavigation />
+        </MainLayout>
+      </TokenRefresher>
     </>
   );
 };
