fab9227094a9282112362a795c5bd4cfa7e709f9
eunjju2 <graceun309@gmail.com>
2025-01-16 14:57:00 +0900

    ♻️ Refactor : 토큰 만료 기존 인스턴스에서 확인하도록 수정 #164

--- src/pages/UserMypage/index.tsx
+++ src/pages/UserMypage/index.tsx
@@ -1,23 +1,20 @@
 import HeaderNoTitle from '@layouts/HeaderNoTitle';
 import MainLayout from '@layouts/MainLayout';
 import BottomNavigation from '@layouts/BottomNavigation';
-import TokenRefresher from '@components/TokenRefresher';
 import UserButtonContainer from './components/UserButtonContainer';
 import UserInfo from './components/UserInfo';
 
 const UserMypage = () => {
   return (
     <>
-      <TokenRefresher>
-        <MainLayout>
-          <HeaderNoTitle />
-          <div className='flex h-[263px] w-[375px] flex-col items-center bg-primary'>
-            <UserInfo />
-            <UserButtonContainer />
-          </div>
-          <BottomNavigation />
-        </MainLayout>
-      </TokenRefresher>
+      <MainLayout>
+        <HeaderNoTitle />
+        <div className='flex h-[263px] w-[375px] flex-col items-center bg-primary'>
+          <UserInfo />
+          <UserButtonContainer />
+        </div>
+        <BottomNavigation />
+      </MainLayout>
     </>
   );
 };
